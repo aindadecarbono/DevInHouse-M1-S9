815384468a0ae2588a23657eb0cae752c39658b0
Bruno Coelho <brunocoelhonet@gmail.com>
2022-05-06 19:06:11 -0300

    ex 3

---
+++ exercicio03.js
@@ -0,0 +1,6 @@
+var aluno;
+aluno.nome = "Bruno";
+aluno.matricula = 12345;
+aluno.ativo = true;
+aluno.cadeiras = ['Algoritmo', 'Cálculo', 'Matemática Discreta', 'Geometria Plana'];
+console.log(aluno);
---
+++ exercicio03.ts
@@ -0,0 +1,13 @@
+
+	let aluno:{
+    nome: string,
+    matricula: number,
+    ativo: boolean,
+    cadeiras: string[] }
+
+    aluno.nome = "Bruno"
+    aluno.matricula = 12345
+    aluno.ativo = true
+    aluno.cadeiras = ['Algoritmo', 'Cálculo', 'Matemática Discreta', 'Geometria Plana']
+
+    
